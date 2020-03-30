314847c25a726ef2111fc2053851f19e55831a63
Meghan Martin <meghanxmartin@gmail.com>
2020-03-30 16:01:35 +0200

    adds logo

--- src/components/header.js
+++ src/components/header.js
@@ -2,12 +2,11 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import './Header.css'
-// import logo from '../images/logo.svg'
 
 const Header = ({ siteTitle }) => (
   <div className="Header">
     <div className="HeaderGroup">
-    <Link to="/"><img src="{}" width="30" /></Link>
+    <Link to="/"><img width="30" src={require('../images/logo.svg')} /></Link>
     <Link to="/about">About Me</Link>
     <Link to="/projects">Projects</Link>
     <Link to="/contact">Contact</Link>
