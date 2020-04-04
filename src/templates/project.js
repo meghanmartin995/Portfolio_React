09a161a01e78941738d63a1971b1cb7a35df9ec3
Meghan Martin <meghanxmartin@gmail.com>
2020-04-04 16:33:09 +0200

    adds data to projects page

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { graphql } from 'gatsby'
 
-const Blog = () => {
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: {slug: { eq: $slug }}) {
+      frontmatter {
+        title
+      }
+      html
+    }
+  }
+`
+const Project = (props) => {
   return (
     <Layout>
-    This is the project template
+    <h1>{props.data.markdownRemark.frontmatter.title} </h1>
+    <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}>
+    </div>
     </Layout>
 
   )
 }
 
-export default Blog
+export default Project
