c78786451f9cf2606fb59554765dedce7ab44a27
Meghan Martin <meghanxmartin@gmail.com>
2020-04-09 11:40:45 +0200

    removes unnecessary components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,8 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
-import Card from '../components/Card';
 import Section from '../components/Section';
-import Wave from '../components/Wave';
 import Projects from '../components/Projects'
 
 
