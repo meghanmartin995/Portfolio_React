2743414bf5ef30cf598dee8bf70610c9eccc83ea
Meghan Martin <meghanxmartin@gmail.com>
2020-04-10 19:29:59 +0200

    changes color on about

--- src/components/Section.js
+++ src/components/Section.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import Wave from '../components/Wave';
 
 const SectionGroup = styled.div `
-  background: #f59d9ec2;
+  background: linear-gradient(180deg, #FF6770 0%, rgba(254, 240, 244, 0.59) 100%);
   height: 1000px;
   background-size: cover;
   display: grid;
